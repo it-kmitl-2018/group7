93d2888f96eed5285d51e4acf5fe9391d2ccc152
BeedleEiEi <58070023@kmitl.ac.th>
2018-03-26 22:45:31 +0700

    Edit TaxinvoiceController
    - Remove unnecessaries comment
    - Edit Mapping method

--- src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
@@ -1,52 +1,44 @@
 package th.ac.kmitl.it.soa.group7.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
+import th.ac.kmitl.it.soa.group7.models.HeaderContext;
 import th.ac.kmitl.it.soa.group7.models.TaxInvoiceModel;
 
-@Controller // ใช้ Controller เพื่อส่ง HTML กลับมา
+@Controller
 public class TaxInvoiceController {
-	//TODO Mapping Controller of XML, JSON Converter
-	private TaxInvoiceModel taxInvoiceModel = new TaxInvoiceModel(); // ให้ Model คอยรับค่า
-	private JsonConverter JsonConverter = new JsonConverter();
 
-	@RequestMapping("/")
-	public String index() {// หน้าแรก return เป็น taxinvoice.html ให้
-		return "taxinvoice.html";
+	@GetMapping("/")
+	public String index() {
+		return "taxinvoice";
 	}
 
-	@RequestMapping(value = "/submitForm", method = {RequestMethod.GET, RequestMethod.POST}) // ถ้าใช้ ResponseBody จะแสดง param
-	// Get parameter
-	public String getResponse(@RequestParam("documentTypeCode") String documentTypeCode, ModelMap modelMap) {
-		taxInvoiceModel.setDocumentTypeCode(documentTypeCode);
-		modelMap.put("documentTypeCode", documentTypeCode);
-		return "confirm.jsp";
-	}
-
-	@RequestMapping(value = "/confirmGenerate")
-	@ResponseBody
-	public String showDetails() {
-		return taxInvoiceModel.getDocumentTypeCode() + " <<<< confirm!";
-	}
-
-	@RequestMapping(value = "/generateJSON")
-	public String generateXML(){
-		String jsonData;
-		jsonData = JsonConverter.toJson(taxInvoiceModel.getData());
-		return "jsonview.jsp";
-	}
-
-	@RequestMapping(value = "/generateXML")
-	public String generateXML(){
-		String xmlData;
-		xmlData = XmlConverter.toJson(taxInvoiceModel.getData());
-		return "xmlview.jsp";
+	@PostMapping(path = "/confirm") // GetParameter and set to HeaderContext
+	public String getModel(@RequestParam String manualId, @RequestParam String documentId,
+			@RequestParam String documentName, @RequestParam String documentTypeCode,
+			@RequestParam String issueDateTime, @RequestParam String purpose, @RequestParam String purposeCode,
+			@RequestParam String globalId, @RequestParam String creationDateTime, ModelMap modelMap) {
+		TaxInvoiceModel taxInvoiceModel = new TaxInvoiceModel();
+		HeaderContext headerContext = new HeaderContext();
+		// Set parameter to HeaderContext
+		headerContext.setManualId(manualId);
+		headerContext.setDocumentId(documentId);
+		headerContext.setDocumentName(documentName);
+		headerContext.setDocumentTypeCode(documentTypeCode);
+		headerContext.setIssueDateTime(issueDateTime);
+		headerContext.setPurpose(purpose);
+		headerContext.setPurposeCode(purposeCode);
+		headerContext.setGlobalId(globalId);
+		headerContext.setCreationDateTime(creationDateTime);
+		// End here
+		taxInvoiceModel.setHeaderContext(headerContext);
+		modelMap.addAttribute(taxInvoiceModel);
+		modelMap.addAttribute("taxModel", headerContext);
+		return "confirm";
 	}
 
 }
