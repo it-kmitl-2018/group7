0d0591b5375867310a58636e457407c6559b166c
BeedleEiEi <58070023@kmitl.ac.th>
2018-04-23 19:10:31 +0700

    Refactor TaxInvoiceController
    Remove code and rewrite in easier way

--- src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
@@ -1,13 +1,12 @@
 package th.ac.kmitl.it.soa.group7.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import th.ac.kmitl.it.soa.group7.models.HeaderContext;
-import th.ac.kmitl.it.soa.group7.models.TaxInvoiceModel;
 
 @Controller
 public class TaxInvoiceController {
@@ -17,27 +16,14 @@ public class TaxInvoiceController {
 		return "taxinvoice";
 	}
 
+	@GetMapping()
+	public String create() {
+		return "taxinvoice";
+	}
+
 	@PostMapping(path = "/confirm") // GetParameter and set to HeaderContext
-	public String getModel(@RequestParam String manualId, @RequestParam String documentId,
-			@RequestParam String documentName, @RequestParam String documentTypeCode,
-			@RequestParam String issueDateTime, @RequestParam String purpose, @RequestParam String purposeCode,
-			@RequestParam String globalId, @RequestParam String creationDateTime, ModelMap modelMap) {
-		TaxInvoiceModel taxInvoiceModel = new TaxInvoiceModel();
-		HeaderContext headerContext = new HeaderContext();
-		// Set parameter to HeaderContext
-		headerContext.setManualId(manualId);
-		headerContext.setDocumentId(documentId);
-		headerContext.setDocumentName(documentName);
-		headerContext.setDocumentTypeCode(documentTypeCode);
-		headerContext.setIssueDateTime(issueDateTime);
-		headerContext.setPurpose(purpose);
-		headerContext.setPurposeCode(purposeCode);
-		headerContext.setGlobalId(globalId);
-		headerContext.setCreationDateTime(creationDateTime);
-		// End here
-		taxInvoiceModel.setHeaderContext(headerContext);
-		modelMap.addAttribute(taxInvoiceModel);
-		modelMap.addAttribute("taxModel", headerContext);
+	public String create(@RequestParam Map<String, String> allRequestParam, Model model) {
+		model.addAllAttributes(allRequestParam);
 		return "confirm";
 	}
 
