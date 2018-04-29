634b59978d781266d94e55696391ca53cb3e3a1e
BeedleEiEi <58070023@kmitl.ac.th>
2018-04-29 21:51:51 +0700

    Add TaxInvoiceControllerTests

---
+++ src/test/java/th/ac/kmitl/it/soa/group7/TaxInvoiceControllerTests.java
@@ -0,0 +1,41 @@
+package th.ac.kmitl.it.soa.group7;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.ModelMap;
+
+import th.ac.kmitl.it.soa.group7.controllers.TaxInvoiceController;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaxInvoiceControllerTests {
+	TaxInvoiceController taxInvoiceController = new TaxInvoiceController();
+
+	@Test
+	public void inputParameterWillGetValidParameter() {
+		// True
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("username", "user1");
+		ModelMap model = new ModelMap();
+		model.addAllAttributes(map);
+		taxInvoiceController.create(map, model);
+		assertThat(model.get("username")).isEqualTo("user1");
+	}
+
+	@Test
+	public void inputParameterWillGetInvalidParameter() {
+		// False
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("username", "user555");
+		ModelMap model = new ModelMap();
+		taxInvoiceController.create(map, model);
+		assertThat(model.get("username")).isNotEqualTo("user1");
+	}
+}
