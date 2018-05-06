d09f1096efcc7d119ce534ab4d681e58b41e73dd
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-06 13:32:05 +0700

    Create SellerInformation
    Collect information of seller

---
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/SellerInformation.java
@@ -0,0 +1,68 @@
+package th.ac.kmitl.it.soa.group7.models;
+
+public class SellerInformation {
+	public String sellerTaxId;
+	public String storeBranch;
+	public String sellerNameEng;
+	public String sellerNameTh;
+	public String sellerEmail;
+	public String sellerTelephone;
+	public String sellerFax;
+
+	public String getSellerTaxId() {
+		return sellerTaxId;
+	}
+
+	public void setSellerTaxId(String sellerId) {
+		this.sellerTaxId = sellerId;
+	}
+
+	public String getStoreBranch() {
+		return storeBranch;
+	}
+
+	public void setStoreBranch(String storeBranch) {
+		this.storeBranch = storeBranch;
+	}
+
+	public String getSellerNameEng() {
+		return sellerNameEng;
+	}
+
+	public void setSellerNameEng(String sellerNameEng) {
+		this.sellerNameEng = sellerNameEng;
+	}
+
+	public String getSellerNameTh() {
+		return sellerNameTh;
+	}
+
+	public void setSellerNameTh(String sellerNameTh) {
+		this.sellerNameTh = sellerNameTh;
+	}
+
+	public String getSellerEmail() {
+		return sellerEmail;
+	}
+
+	public void setSellerEmail(String sellerEmail) {
+		this.sellerEmail = sellerEmail;
+	}
+
+	public String getSellerTelephone() {
+		return sellerTelephone;
+	}
+
+	public void setSellerTelephone(String sellerTelephone) {
+		this.sellerTelephone = sellerTelephone;
+	}
+
+	public String getSellerFax() {
+		return sellerFax;
+	}
+
+	public void setSellerFax(String sellerFax) {
+		this.sellerFax = sellerFax;
+	}
+
+}
