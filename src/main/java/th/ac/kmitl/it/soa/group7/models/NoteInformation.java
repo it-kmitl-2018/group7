b3f0a429a64356d0435605bc10acfccdae37f196
Nattapon Chittnarong <smarty0123@users.noreply.github.com>
2018-05-06 19:35:27 +0700

    create NoteInformation

---
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/NoteInformation.java
@@ -0,0 +1,23 @@
+package th.ac.kmitl.it.soa.group7.models;
+
+public class NoteInformation {
+	private String subject;
+	private String content;
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+}
