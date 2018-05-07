3d8c7b1644eb4d99e9c74648c89c54052f32597a
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-07 18:47:49 +0700

    Implement buyer, seller, address
    Implement these class for json

--- src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInvoiceModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInvoiceModel.java
@@ -1,16 +1,43 @@
 package th.ac.kmitl.it.soa.group7.models;
 
-public class TaxInvoiceModel {// Collect Tax Invoice details
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaxInvoiceModel {
 	public TaxInvoiceModel() {
+		this.sellerInfo = new SellerInformation();
+		this.buyerInfo = new BuyerInformation();
+		this.sellerAddressInformation = sellerInfo.getAddressInformation();
+		this.buyerAddressInformation = buyerInfo.getAddressInformation();
+		this.list = new ArrayList<Object>();
 	}
 
+	private TaxInvoiceModel taxInvoiceModel;
 	private String documentTypeCode;
+	private SellerInformation sellerInfo;
+	private BuyerInformation buyerInfo;
+	private AddressInformation sellerAddressInformation;
+	private AddressInformation buyerAddressInformation;
+	private List<Object> list;
 
-	public void setDocumentTypeCode(String documentTypeCode) {// Set typeCode
+	public List<Object> getItem() {
+		return list;
+	}
+
+	public Object getTaxInvoice() {
+		return taxInvoiceModel;
+	}
+
+	public void setDocumentTypeCode(String documentTypeCode) {
 		this.documentTypeCode = documentTypeCode;
 	}
 
-	public String getDocumentTypeCode() {// Get typeCode
+	public String getDocumentTypeCode() {
 		return documentTypeCode;
+	}
+
+	public void setItem(BuyerInformation buyerInfo, SellerInformation sellerInfo) {
+		this.list.add(sellerInfo);
+		this.list.add(buyerInfo);
 	}
 }
