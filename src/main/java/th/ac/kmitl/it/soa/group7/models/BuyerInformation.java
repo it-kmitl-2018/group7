7afc82a4a69a25e04664f61c99495633b888c92c
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-07 18:46:59 +0700

    Add address into class
    For being call in json

--- src/main/java/th/ac/kmitl/it/soa/group7/models/BuyerInformation.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/BuyerInformation.java
@@ -8,61 +8,70 @@ public class BuyerInformation {
 	public String email;
 	public String telephone;
 	public String fax;
-	
+	public AddressInformation addressInformation;
+
+	public void setAddressInformation(AddressInformation addressInformation) {
+		this.addressInformation = addressInformation;
+	}
+
+	public AddressInformation getAddressInformation() {
+		return this.addressInformation;
+	}
+
 	public String getTaxId() {
 		return taxId;
 	}
-	
+
 	public void setTaxId(String taxId) {
 		this.taxId = taxId;
 	}
-	
+
 	public String getStoreBranch() {
 		return storeBranch;
 	}
-	
+
 	public void setStoreBranch(String storeBranch) {
 		this.storeBranch = storeBranch;
 	}
-	
+
 	public String getNameEng() {
 		return nameEng;
 	}
-	
+
 	public void setNameEng(String nameEng) {
 		this.nameEng = nameEng;
 	}
-	
+
 	public String getNameTh() {
 		return nameTh;
 	}
-	
+
 	public void setNameTh(String nameTh) {
 		this.nameTh = nameTh;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getTelephone() {
 		return telephone;
 	}
-	
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-	
+
 	public String getFax() {
 		return fax;
 	}
-	
+
 	public void setFax(String fax) {
 		this.fax = fax;
 	}
-	
+
 }
