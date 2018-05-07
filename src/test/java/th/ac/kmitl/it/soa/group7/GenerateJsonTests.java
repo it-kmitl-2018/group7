02892ecda12145a85d2d3a9b197b3e2cc21955aa
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-07 22:31:56 +0700

    Edit tests

--- src/test/java/th/ac/kmitl/it/soa/group7/GenerateJsonTests.java
+++ src/test/java/th/ac/kmitl/it/soa/group7/GenerateJsonTests.java
@@ -1,64 +1,39 @@
 package th.ac.kmitl.it.soa.group7;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import th.ac.kmitl.it.soa.group7.converters.JsonConverter;
-import th.ac.kmitl.it.soa.group7.models.AddressInformation;
-import th.ac.kmitl.it.soa.group7.models.BuyerInformation;
-import th.ac.kmitl.it.soa.group7.models.SellerInformation;
-import th.ac.kmitl.it.soa.group7.models.TaxInvoiceModel;
 
 public class GenerateJsonTests {
+	private class Json {
+
+        @JsonProperty("strings")
+        private List<String> strings = Arrays.asList("a", "b", "c");
+    }
+	
 	@Test
 	public void GenerateObjectToJson() {
-		JsonConverter jsc = new JsonConverter();
-		SellerInformation sli = new SellerInformation();
-		BuyerInformation bli = new BuyerInformation();
-		TaxInvoiceModel tax = new TaxInvoiceModel();
-		AddressInformation sellerAddressInfo = new AddressInformation();
-		AddressInformation buyerAddressInfo = new AddressInformation();
-
-		// Set seller address
-		sellerAddressInfo.setAddressLineOne("สำนักงานใหญ่ เลขที่ 313/1");
-		sellerAddressInfo.setAddressLineTwo("ถนนรัชดาภิเษก แขวงบุคลโล เขตธนบุรี กรุงเทพมหานคร 10600");
-		sellerAddressInfo.setCityName("ธนบุรี");
-		sellerAddressInfo.setCitySubDivision("บุคคโล");
-		sellerAddressInfo.setPostCode("10600");
-		sellerAddressInfo.setCountrySubDivision("กรุงเทพมหานคร");
-		sellerAddressInfo.setCountryName("TH");
-		// Set buyer address
-		buyerAddressInfo.setAddressLineOne("500 ถนนเพลินจิต");
-		buyerAddressInfo.setAddressLineTwo("แขวงลุมพินี เขตปทุมวัน กรุงเทพมหานคร 10330");
-		buyerAddressInfo.setCityName("ปทุมวัน");
-		buyerAddressInfo.setCitySubDivision("ลุมพินี");
-		buyerAddressInfo.setPostCode("10330");
-		buyerAddressInfo.setCountrySubDivision("กรุงเทพมหานคร");
-		buyerAddressInfo.setCountryName("TH");
-		// Set buyer informations
-		bli.setTaxId("0107536000986");
-		bli.setStoreBranch("00000");
-		bli.setNameEng("Kiatnakin Bank Public Company Limited");
-		bli.setNameTh("ธนาคารเกียรตินาคิน จำกัด (มหาชน)");
-		bli.setEmail("contact@kiatnakin.co.th");
-		bli.setTelephone("02-1655555");
-		bli.setFax("02-1655555");
-		bli.setAddressInformation(buyerAddressInfo);
-		// Set seller informations
-		sli.setTaxId("INV-123456");
-		sli.setBranch("00000");
-		sli.setNameEng("Beedle");
-		sli.setNameTh("ไทยแลนด์");
-		sli.setEmail("hello@mail.com");
-		sli.setTelephone("06-5507-9511");
-		sli.setFax("06-5507-9511");
-		sli.setAddressInformation(sellerAddressInfo);
-		// Add to Tax invoice model
-		tax.setItem(bli, sli);
-
-		Assert.assertNotNull(tax);
-		Assert.assertEquals(jsc.toJson("seller, A, B, C"), '"' + "seller, A, B, C" + '"');
-
-		System.out.println(jsc.toJson(tax));
+		JsonConverter jasonConverter = new JsonConverter();
+		Assert.assertEquals(jasonConverter.toJson("seller, A, B, C"), '"' + "seller, A, B, C" + '"');
 	}
+	
+    @Test
+    public void shouldGetCorrectJson() {
+    	String json =
+                "{\n" +
+                "    \"strings\" : [\n" +
+                "        \"a\",\n" +
+                "        \"b\",\n" +
+                "        \"c\"\n" +
+                "    ]\n" +
+                "}";
+        JsonConverter jsonConverter = new JsonConverter();
+        Assert.assertEquals(json, jsonConverter.toJson(new Json()));
+    }
 }
