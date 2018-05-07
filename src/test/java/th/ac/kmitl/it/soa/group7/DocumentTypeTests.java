7090245d6ef882e4c1191db58dcdeb3bd51d73e2
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-07 16:31:15 +0700

    Edit test after installed lombok

--- src/test/java/th/ac/kmitl/it/soa/group7/DocumentTypeTests.java
+++ src/test/java/th/ac/kmitl/it/soa/group7/DocumentTypeTests.java
@@ -8,7 +8,11 @@ import th.ac.kmitl.it.soa.group7.definitions.DocumentType;
 public class DocumentTypeTests {
 	@Test
 	public void DocumentTypeOutputMatchTest() {
-		assertEquals(DocumentType.DEBIT_NOTE.getDocumentCode(), "80");
-
+		Assert.assertEquals(DocumentType.DEBIT_NOTE.getDocumentCode(), "80");
+		Assert.assertEquals(DocumentType.DEBIT_NOTE.getDocumentName(), "ใบเพิ่มหนี้ (Debit note)");
+		Assert.assertEquals(DocumentType.CREDIT_NOTE.getDocumentCode(), "81");
+		Assert.assertEquals(DocumentType.CREDIT_NOTE.getDocumentName(), "ใบลดหนี้ (Credit note)");
+		Assert.assertEquals(DocumentType.INVOICE.getDocumentCode(), "380");
+		Assert.assertEquals(DocumentType.INVOICE.getDocumentName(), "ใบแจ้งหนี้ (Invoice)");
 	}
 }
