22a1e2a9e51cc6e9a30a81e428c88e99ee8f1cec
Nattapon Chittnarong <smarty0123@users.noreply.github.com>
2018-05-13 04:21:08 +0700

    create AllowanceChargeCode

---
+++ src/test/java/th/ac/kmitl/it/soa/group7/AllowanceChargeCodeTests.java
@@ -0,0 +1,25 @@
+package th.ac.kmitl.it.soa.group7;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import th.ac.kmitl.it.soa.group7.definitions.AllowanceChargeCode;
+
+public class AllowanceChargeCodeTests {
+	@Test
+	public void allowanceChargeCodeOutputMatchTest() {
+		Assert.assertEquals(AllowanceChargeCode.STAMP_DUTY.getCode(), 28);
+		Assert.assertEquals(AllowanceChargeCode.BANK_CHARGES.getCode(), 30);
+		Assert.assertEquals(AllowanceChargeCode.MINIMUM_COMMISSION.getCode(), 52);
+		Assert.assertEquals(AllowanceChargeCode.TRANSFER_CHARGES.getCode(), 55);
+		Assert.assertEquals(AllowanceChargeCode.MISCELLANEOUS_CHARGES.getCode(), 57);
+		Assert.assertEquals(AllowanceChargeCode.FOREIGN_EXCHANGE_CHARGES.getCode(), 58);
+		Assert.assertEquals(AllowanceChargeCode.SPECIAL_AGREEMENT_CHARGE.getCode(), 65);
+		Assert.assertEquals(AllowanceChargeCode.SAMPLE_DISCOUNT.getCode(), 66);
+		Assert.assertEquals(AllowanceChargeCode.RUSH_DELIVERY_SURCHARGE.getCode(), 77);
+		Assert.assertEquals(AllowanceChargeCode.LOADING_CHARGE.getCode(), 82);
+		Assert.assertEquals(AllowanceChargeCode.WAREHOUSING_CHARGE.getCode(), 85);
+		Assert.assertEquals(AllowanceChargeCode.REDUCTION_FROM_USUAL_PRICE.getCode(), 95);
+		Assert.assertEquals(AllowanceChargeCode.CHARGE_FOR_INSURANCE.getCode(), 96);
+	}
+}
