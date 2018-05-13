a5321485a902958e1c7a690643f4a01efe508cdc
noxusfrosted <ntpm96@hotmail.com>
2018-05-13 16:31:10 +0700

    Add create json test

--- src/test/java/th/ac/kmitl/it/soa/group7/models/json/AddressJsonConverterTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group7/models/json/AddressJsonConverterTest.java
@@ -3,6 +3,7 @@ package th.ac.kmitl.it.soa.group7.models.json;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import th.ac.kmitl.it.soa.group7.converters.JsonConverter;
 
 class AddressJsonConverterTest {
 
@@ -26,12 +27,43 @@ class AddressJsonConverterTest {
                 .countryName(this.countryName)
                 .build();
 
-        assertEquals(this.lineOne , address.lineOne);
-        assertEquals(this.lineTwo , address.lineTwo);
-        assertEquals(this.cityName , address.cityName);
-        assertEquals(this.citySubDivisionName , address.citySubDivisionName);
-        assertEquals(this.postCode , address.postCode);
-        assertEquals(this.countrySubDivision , address.countrySubDivision);
-        assertEquals(this.countryName , address.countryName);
+        assertEquals(this.lineOne, address.lineOne);
+        assertEquals(this.lineTwo, address.lineTwo);
+        assertEquals(this.cityName, address.cityName);
+        assertEquals(this.citySubDivisionName, address.citySubDivisionName);
+        assertEquals(this.postCode, address.postCode);
+        assertEquals(this.countrySubDivision, address.countrySubDivision);
+        assertEquals(this.countryName, address.countryName);
     }
+
+    @Test
+    public void shouldGetCorrectJson() {
+
+        AddressJsonConverter address = AddressJsonConverter.builder()
+                .lineOne(this.lineOne)
+                .lineTwo(this.lineTwo)
+                .cityName(this.cityName)
+                .citySubDivisionName(this.citySubDivisionName)
+                .postCode(this.postCode)
+                .countrySubDivision(this.countrySubDivision)
+                .countryName(this.countryName)
+                .build();
+
+        String json
+                = "{\n"
+                + "    \"line_one\": \"สำนักงานใหญ่ เลขที่ 313/1\",\n"
+                + "    \"line_two\": \"ถนนรัชดาภิเษก แขวงบุคลโล เขตธนบุรี "
+                + "กรุงเทพมหานคร 10600\",\n"
+                + "    \"city_name\": \"ธนบุรี\",\n"
+                + "    \"city_sub_division_name\": \"บุคคโล\",\n"
+                + "    \"post_code\": \"10600\",\n"
+                + "    \"country_sub_division\": \"กรุงเทพมหานคร\",\n"
+                + "    \"country_name\": \"TH\"\n"
+                + "}";
+
+        JsonConverter jsonConverter = new JsonConverter();
+
+        assertEquals(json, jsonConverter.toJson(address));
+    }
+
 }
