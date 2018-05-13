9c8f60fdcdfdff118a037b636e308a3642d794b7
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-14 02:28:51 +0700

    Create IndividualTradeProductInstanceTests

---
+++ src/test/java/th/ac/kmitl/it/soa/group7/IndividualTradeProductInstanceXMLTests.java
@@ -0,0 +1,21 @@
+package th.ac.kmitl.it.soa.group7;
+
+import java.sql.Timestamp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import th.ac.kmitl.it.soa.group7.models.xml.IndividualTradeProductInstanceXML;
+
+public class IndividualTradeProductInstanceXMLTests {
+	private String batchId = "0065";
+	private Timestamp expiryDateTime = new Timestamp(System.currentTimeMillis());
+
+	@Test
+		IndividualTradeProductInstanceXML individualTradeProductInstance = IndividualTradeProductInstanceXML.builder()
+				.batchId(batchId).expiryDateTime(expiryDateTime).build();
+
+		Assert.assertEquals(individualTradeProductInstance.batchId, this.batchId);
+		Assert.assertEquals(individualTradeProductInstance.expiryDateTime, this.expiryDateTime);
+	}
+}
