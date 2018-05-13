a33c4405082598ff5e0e4a924c11b21e4ca2f87d
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-14 00:21:21 +0700

    Add XmlRootElement

--- src/main/java/th/ac/kmitl/it/soa/group7/models/xml/SpecifiedTaxRegistrationXML.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/xml/SpecifiedTaxRegistrationXML.java
@@ -1,10 +1,13 @@
 package th.ac.kmitl.it.soa.group7.models.xml;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
 import lombok.Builder;
 
 @Builder
+@JacksonXmlRootElement(localName = "ram:SpecifiedTaxRegistration")
 public class SpecifiedTaxRegistrationXML {
-	@JacksonXmlProperty(localName = "ram:SpecifiedTaxRegistration")
+	@JacksonXmlProperty(localName = "ram:ID")
 	public String id;
 }
