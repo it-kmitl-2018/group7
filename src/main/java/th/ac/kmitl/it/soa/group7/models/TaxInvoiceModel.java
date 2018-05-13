f0d3e8a23916c77fbf1c3839189f0aa7de57005a
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-13 17:55:29 +0700

    Implement BuyerInformation into TaxInvoiceModel

--- src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInvoiceModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInvoiceModel.java
@@ -1,16 +1,24 @@
 package th.ac.kmitl.it.soa.group7.models;
 
-public class TaxInvoiceModel {// Collect Tax Invoice details
+public class TaxInvoiceModel {
 	public TaxInvoiceModel() {
+		this.buyerInfo = new BuyerInformation();
+		this.buyerAddressInformation = buyerInfo.getAddressInformation();
 	}
 
+	private BuyerInformation buyerInfo;
+	private AddressInformation buyerAddressInformation;
 	private String documentTypeCode;
 
-	public void setDocumentTypeCode(String documentTypeCode) {// Set typeCode
+	public BuyerInformation getBuyer() {
+		return buyerInfo;
+	}
+
+	public void setDocumentTypeCode(String documentTypeCode) {
 		this.documentTypeCode = documentTypeCode;
 	}
 
-	public String getDocumentTypeCode() {// Get typeCode
+	public String getDocumentTypeCode() {
 		return documentTypeCode;
 	}
 }
