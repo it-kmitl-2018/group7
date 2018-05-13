2d81ded475b6001d838157d4383ab25079d6a1e2
Nattapon Chittnarong <smarty0123@users.noreply.github.com>
2018-05-13 12:40:15 +0700

    refactor space

--- src/main/java/th/ac/kmitl/it/soa/group7/definitions/TaxpayerIdCode.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/definitions/TaxpayerIdCode.java
@@ -4,13 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum TaxpayerIdCode {
-	
-	TAX_ID("TXID", "เลขประจำตัวผู้เสียภาษีอากรสำหรับนิติบุคคล 13 หลัก รวมเลขสาขาอีก 5 หลัก", "Tax ID"),
+    TAX_ID("TXID", "เลขประจำตัวผู้เสียภาษีอากรสำหรับนิติบุคคล 13 หลัก รวมเลขสาขาอีก 5 หลัก", "Tax ID"),
     ID_CARD_NUMBER("NIDN", "เลขประจำตัวประชาชน 13 หลัก (สำหรับบุคคลธรรมดา)", "National Registration Identification Number"),
     PASSPORT_NUMBER("CCPT", "เลขหนังสือเดินทาง (Passport)", "Number assigned by an authority to identify the passport number of a person"),
     OTHER_ID_SCHEMA("OTHR", "ตัวระบุอื่นๆใช้ในการระบุเอกลักษณ์ผู้ซื้อนอกเหนือจาก Scheme TXID NIDN CCPT", "Other Identification scheme");
 	
-	public String taxpayerIdCode;
+    public String taxpayerIdCode;
     public String nameTh;
     public String nameEn;
     
