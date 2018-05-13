3908f1b3583182aa08d66f71b5beb1d5b17548e7
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-13 17:39:46 +0700

    Implement SellerInformation into TaxInvoiceModel
    Implent SellerInformaion into TaxInvoiceModel for JsonFormat

--- src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInvoiceModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInvoiceModel.java
@@ -1,16 +1,24 @@
 package th.ac.kmitl.it.soa.group7.models;
 
-public class TaxInvoiceModel {// Collect Tax Invoice details
+public class TaxInvoiceModel {
 	public TaxInvoiceModel() {
+		this.sellerInfo = new SellerInformation();
+		this.sellerAddressInformation = sellerInfo.getAddressInformation();
 	}
 
+	private SellerInformation sellerInfo;
+	private AddressInformation sellerAddressInformation;
 	private String documentTypeCode;
 
-	public void setDocumentTypeCode(String documentTypeCode) {// Set typeCode
+	public void setDocumentTypeCode(String documentTypeCode) {
 		this.documentTypeCode = documentTypeCode;
 	}
 
-	public String getDocumentTypeCode() {// Get typeCode
+	public String getDocumentTypeCode() {
 		return documentTypeCode;
+	}
+
+	public SellerInformation getSeller() {
+		return sellerInfo;
 	}
 }
