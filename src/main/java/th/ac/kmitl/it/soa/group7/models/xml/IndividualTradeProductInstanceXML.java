6ddfdc7f045499217f25397978cf97fc5c665105
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-14 02:27:51 +0700

    Create IndividualTradeProductInstanceXML

---
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/xml/IndividualTradeProductInstanceXML.java
@@ -0,0 +1,17 @@
+package th.ac.kmitl.it.soa.group7.models.xml;
+
+import java.sql.Timestamp;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+import lombok.Builder;
+
+@Builder
+@JacksonXmlRootElement(localName = "ram:IndividualTradeProductInstance")
+public class IndividualTradeProductInstanceXML {
+	@JacksonXmlProperty(localName = "ram:BatchID")
+	public String batchId;
+	@JacksonXmlProperty(localName = "ram:ExpiryDateTime")
+	public Timestamp expiryDateTime;
+}
