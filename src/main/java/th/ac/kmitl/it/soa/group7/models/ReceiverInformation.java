8dc389d0e8e0725e93652384d694c9457a180846
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-13 21:46:44 +0700

    Add Email and Telephone

--- src/main/java/th/ac/kmitl/it/soa/group7/models/ReceiverInformation.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/ReceiverInformation.java
@@ -6,5 +6,7 @@ public @Data class ReceiverInformation {
 	private String id;
 	private String name;
 	private String branch;
+	private String email;
+	private String telePhone;
 	private AddressInformation addressInformation;
 }
