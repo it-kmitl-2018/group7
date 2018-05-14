7d526e35a1567588e4a273e26ff682cdbcc461fb
niot1112 <chanchai.nice33@gmail.com>
2018-05-14 12:06:58 +0700

    change to lombok
    refactor to lombok

--- src/main/java/th/ac/kmitl/it/soa/group7/models/ReferenceDocumentModel.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/ReferenceDocumentModel.java
@@ -1,31 +1,10 @@
 package th.ac.kmitl.it.soa.group7.models;
 
+import lombok.Data;
+
+@Data
 public class ReferenceDocumentModel {
     private String referenceDocumentId;
     private String referenceDocumentDate;
     private String referenceDocumentTypeCode;
-
-    public String getReferenceDocId() {
-        return referenceDocumentId;
-    }
-
-    public void setReferenceDocId(String ReferenceDocId) {
-        this.referenceDocumentId = ReferenceDocId;
-    }
-
-    public String getReferenceDocDate() {
-        return referenceDocumentDate;
-    }
-
-    public void setReferenceDocDate(String ReferenceDocDate) {
-        this.referenceDocumentDate = ReferenceDocDate;
-    }
-
-    public String getReferenceDocTypeCode() {
-        return referenceDocumentTypeCode;
-    }
-
-    public void setReferenceDocTypeCode(String ReferenceDocTypeCode) {
-        this.referenceDocumentTypeCode = ReferenceDocTypeCode;
-    }
 }
