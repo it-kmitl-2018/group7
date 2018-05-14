8eee58a47b2a58a909a979d60dddc7be605005f3
niot1112 <chanchai.nice33@gmail.com>
2018-05-14 11:15:54 +0700

    refactor to lombok
    change to lombok

--- src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInfomation.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/models/TaxInfomation.java
@@ -1,33 +1,10 @@
 package th.ac.kmitl.it.soa.group7.models;
 
+import lombok.Data;
+
+@Data
 public class TaxInfomation {
     private String taxCode;
     private int calculatedRate;
     private String basisAmount;
-
-    public String getTaxCode() {
-        return taxCode;
-    }
-
-    public void setTaxCode(String taxCode) {
-        this.taxCode = taxCode;
-    }
-
-    public int getCalculatedRate() {
-        return calculatedRate;
-    }
-
-    public void setCalculatedRate(int calculatedRate) {
-        this.calculatedRate = calculatedRate;
-    }
-
-    public String getBasisAmount() {
-        return basisAmount;
-    }
-
-    public void setBasisAmount(String basisAmount) {
-        this.basisAmount = basisAmount;
-    }
-    
-    
 }
