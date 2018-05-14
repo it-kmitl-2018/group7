0b3db293890ba9a4545106a2379c540321f951e0
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-14 22:26:27 +0700

    Add mapping in controller
    Add variable path /generate/individualTradeProductInstanceXML in
    controller

--- src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import th.ac.kmitl.it.soa.group7.models.xml.IndividualTradeProductInstanceXML;
 
 @Controller
 public class TaxInvoiceController {
@@ -27,4 +30,11 @@ public class TaxInvoiceController {
 		return "confirm";
 	}
 
+	@GetMapping(path = "/generate/individualTradeProductInstanceXML")
+	@ResponseBody
+	public String individualTradeProductInstanceXML(@RequestParam Map<String, String> allRequestParam, ModelMap model) {
+		IndividualTradeProductInstanceXML individualTradeProductInstance = IndividualTradeProductInstanceXML.builder()
+				.batchId(allRequestParam.get("id")).expiryDateTime(allRequestParam.get("globalId")).build();
+		return individualTradeProductInstance.toString();
+	}
 }
