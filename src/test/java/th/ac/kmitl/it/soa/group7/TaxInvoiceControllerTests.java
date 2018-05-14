02ca9558381a9c5b1b1ddaabe933131ad9469fdc
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-14 21:18:18 +0700

    Add test in controller
    Add test for mapping /generate to get XML buy need to implement XML
    converter to be completed.

--- src/test/java/th/ac/kmitl/it/soa/group7/TaxInvoiceControllerTests.java
+++ src/test/java/th/ac/kmitl/it/soa/group7/TaxInvoiceControllerTests.java
@@ -43,4 +43,9 @@ public class TaxInvoiceControllerTests {
 		this.mockMvc.perform(get("/confirm")).andDo(print()).andExpect(status().isOk())
 				.andExpect(content().string(containsString("Tax invoice")));
 	}
+
+	@Test
+	public void shouldGenerateXML() throws Exception {
+		this.mockMvc.perform(get("/generate")).andExpect(status().isOk());
+	}
 }
