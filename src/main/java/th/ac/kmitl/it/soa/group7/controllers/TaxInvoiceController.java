0c9b6dcd9875011c29ef1fa65e9ab01dfd004172
BeedleEiEi <58070023@kmitl.ac.th>
2018-05-14 22:56:51 +0700

    Add mapping /generate/ in controller
    Add /generate/specifiedLineTradeDeliveryXML in controller

--- src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
+++ src/main/java/th/ac/kmitl/it/soa/group7/controllers/TaxInvoiceController.java
@@ -7,6 +7,9 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import th.ac.kmitl.it.soa.group7.models.xml.SpecifiedLineTradeDeliveryXML;
 
 @Controller
 public class TaxInvoiceController {
@@ -27,4 +30,12 @@ public class TaxInvoiceController {
 		return "confirm";
 	}
 
+	@GetMapping(path = "/generate/specifiedLineTradeDeliveryXML")
+	@ResponseBody
+	public String specifiedLineTradeDeliveryXML(@RequestParam Map<String, String> allRequestParam, ModelMap model) {
+		SpecifiedLineTradeDeliveryXML specifiedLineTradeDelivery = SpecifiedLineTradeDeliveryXML.builder()
+				.billedQuantity(allRequestParam.get("billedQuantity"))
+				.perPackageUnitQuantity(allRequestParam.get("perPackageUnitQuantity")).build();
+		return specifiedLineTradeDelivery.toString();
+	}
 }
